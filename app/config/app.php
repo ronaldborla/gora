13937441fe9ba2080030ebe5fb59e93d919f20b9
abzdmachinist <iamdmachinist@gmail.com>
2014-10-18 22:52:02 +0800

    Update Generators Way

--- app/config/app.php
+++ app/config/app.php
@@ -121,7 +121,8 @@ return array(
 		'Illuminate\Validation\ValidationServiceProvider',
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
-		'Slushie\LaravelAssetic\LaravelAsseticServiceProvider',
+		'Slushie\LaravelAssetic\LaravelAsseticServiceProvider',		
+		'Way\Generators\GeneratorsServiceProvider',
 
 	),
 
