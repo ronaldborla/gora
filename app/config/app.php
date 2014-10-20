9ab5644391936e95fd9f2a6b13b4ce01ad057163
Ronald Borla <ronald.borla@yahoo.com>
2014-10-20 17:06:40 +0800

    Removed Way/Generators

--- app/config/app.php
+++ app/config/app.php
@@ -122,7 +122,7 @@ return array(
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
 		'Slushie\LaravelAssetic\LaravelAsseticServiceProvider',		
-		'Way\Generators\GeneratorsServiceProvider',
+		//'Way\Generators\GeneratorsServiceProvider',
 
 	),
 
